65397001f76415671115c435af6bee7bf885d8b5
Kevin Lalumiere <kevin.lalumiere@gmail.com>
2016-10-01 22:33:18 -0400

    Introduce the main class StackDeferred
    Documentation remains to be done

---
+++ include/StackDeferred.h
@@ -0,0 +1,43 @@
+#ifndef STACKDEFERRED_H
+#define	STACKDEFERRED_H
+
+#include <cassert>
+#include <type_traits>
+
+template<class Type>
+class StackDeferred {
+public:
+	StackDeferred() = default;
+	StackDeferred(Type x)
+	{ construct(std::move(x)); }
+    StackDeferred(const StackDeferred<Type> &rhs) {
+        if(rhs.data != nullptr) construct(*data);
+    }
+    StackDeferred(StackDeferred<Type> &&rhs) noexcept = default;
+	~StackDeferred() {
+        destruct();
+	}
+    StackDeferred &operator=(const StackDeferred<Type> &rhs) {
+        if(rhs.data != nullptr) construct(*data);
+        return *this;
+    }
+    StackDeferred &operator=(StackDeferred<Type> &&rhs) noexcept = default;
+	void construct(Type x) {
+        assert(data == nullptr);
+		data = new(stackMemory) Type{std::move(x)};
+	}
+	void destruct() {
+		if(data != nullptr) data->~Type();
+        data = nullptr;
+	}
+	Type &get() const {
+		assert(data != nullptr);
+		return *data;
+	}
+
+private:
+	typename std::aligned_storage<sizeof(Type), alignof(Type)>::type stackMemory[1];
+	typename std::add_pointer<Type>::type data = nullptr;
+};
+
+#endif	/* STACKDEFERRED_H */
--- src/StackDeferred_tests.cpp
+++ src/StackDeferred_tests.cpp
@@ -1,5 +1,66 @@
 #include "gtest/gtest.h"
+#include "StackDeferred.h"
 
-TEST(StackDeferred, failing) {
-	FAIL();
+class StackDeferredTests : public ::testing::Test {
+public:
+    struct Destructed {
+        Destructed(int &callCount) : callCount(callCount) {}
+        ~Destructed() { ++callCount; }
+        int &callCount;
+    };
+
+    const int intValue = 3;
+    const int tempObjectDestroyed = 1;
+    const int stackDeferredDestroyed = 1;
+};
+
+
+TEST_F(StackDeferredTests, emptyConstructor) {
+	const StackDeferred<const int> s{};
+}
+TEST_F(StackDeferredTests, get) {
+	const StackDeferred<const int> s(intValue);
+	EXPECT_EQ(intValue,s.get());
+}
+TEST_F(StackDeferredTests, construct) {
+	StackDeferred<const int> s;
+	s.construct(intValue);
+	EXPECT_EQ(intValue,s.get());
+}
+TEST_F(StackDeferredTests, destruct) {
+	int callCount = 0;
+	StackDeferred<Destructed> s;
+	s.construct(Destructed{callCount});
+	s.destruct();
+	EXPECT_EQ(tempObjectDestroyed + stackDeferredDestroyed,callCount);
+}
+TEST_F(StackDeferredTests, constructTwice) {
+    StackDeferred<const int> s;
+    s.construct(intValue);
+    s.destruct();
+    const int newValue = 42;
+    s.construct(newValue);
+    EXPECT_EQ(newValue,s.get());
+}
+TEST_F(StackDeferredTests, destructOutOfScope) {
+    int callCount = 0;
+    {
+        StackDeferred<Destructed> s;
+        s.construct(Destructed{callCount});
+    }
+    EXPECT_EQ(tempObjectDestroyed + stackDeferredDestroyed,callCount);
+}
+TEST_F(StackDeferredTests, classWithInheritance) {
+	struct Base {
+		virtual ~Base() {}
+		int baseVal = 3;
+	};
+	struct Derived : Base {
+		double derivedVal = 2.3;
+	};
+
+	const Derived expected{};
+	const StackDeferred<Derived> s(Derived{});
+	EXPECT_EQ(expected.baseVal,s.get().baseVal);
+	EXPECT_EQ(expected.derivedVal,s.get().derivedVal);
 }
